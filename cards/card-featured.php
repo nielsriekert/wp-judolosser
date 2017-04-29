aafc1fe332f512dd738763d49fd690b8af4ab88c
nielsriekert <niels.riekert@gmail.com>
2017-04-29 21:46:38 +0200

    finished #11 and small changes menu

--- cards/card-featured.php
+++ cards/card-featured.php
@@ -16,7 +16,7 @@ if($locations->have_posts()){
 		$image_src = wp_get_attachment_image_src(get_post_thumbnail_id(), 'card')[0];
 	}
 	?>
-	<section class="card card-featured"<?php if(has_post_thumbnail()){?> style="background-image: url('<?php echo $image_src; ?>');"<?php } ?>>
+	<section class="card card-featured <?php if(has_post_thumbnail()){ ?>card-type-image<?php } ?>"<?php if(has_post_thumbnail()){?> style="background-image: url('<?php echo $image_src; ?>');"<?php } ?>>
 		<a href="<?php the_permalink(); ?>">
 			<div class="card-body">
 				<h2><?php the_title(); ?></h2>
