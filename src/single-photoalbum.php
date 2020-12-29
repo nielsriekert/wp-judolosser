3ddb618b0d32368041075a5e8831d31ab45e7540
Niels Riekert <niels.riekert@gmail.com>
2020-12-29 10:40:45 +0100

    fixed single-photoalbum template

--- src/single-photoalbum.php
+++ src/single-photoalbum.php
@@ -23,8 +23,8 @@ if(has_post_thumbnail()){
 	the_content();
 
 	$photos = array();
-	$photo_thumbs = $photoalbum->getPhotoThumbs();
-	foreach( $photoalbum->getPhotos() as $photo ) {
+	$photo_thumbs = $photo_album->getPhotoThumbs();
+	foreach( $photo_album->getPhotos() as $photo ) {
 		$photos[] = (object) array(
 			'full' => $photo,
 			'thumb' => $photo_thumbs[count( $photos )]
