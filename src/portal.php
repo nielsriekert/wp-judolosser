a8e4847c2e2b7a780882523e1ae78348a8e857ea
Niels Riekert <niels.riekert@gmail.com>
2021-01-01 23:56:11 +0100

    fixed last release :-)

--- src/portal.php
+++ src/portal.php
@@ -9,8 +9,6 @@ while( have_posts() ) { the_post();
 	$cards = CardModel::getCards( get_post() );
 	CardView::viewCards( $cards );
 
-	bdump(EventModel::getEvent(3914)->isPastEvent());
-
 	$content = get_the_content();
 	if( $content ) { ?>
 		<div class="content article">
